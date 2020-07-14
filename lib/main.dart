d0b01cb4af4e0133444de3289ee016952d81547a
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-07-15 02:38:34 +0530

    Added Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'models/Transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,12 +19,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-        id: "t1", title: "Starbucks Coffee", amount: 250, date: DateTime.now()),
-    Transaction(
-        id: "t2", title: "New Shoes", amount: 3000, date: DateTime.now())
-  ];
+  
   // String titleInput, amountInput;
   final titleController = TextEditingController();
   final amountController = TextEditingController();
@@ -75,59 +68,11 @@ class MyHomePage extends StatelessWidget {
                 ],
               ),
             ),
-          ),
-
-          //For a list of Past transactions
-          Column(
-            children: transactions.map(
-              (tx) {
-                return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        child: Text(
-                          "\₹ " + tx.amount.toString(),
-                          style: TextStyle(
-                            color: Colors.purple[400],
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        padding: EdgeInsets.all(10),
-
-                        // decoration: BoxDecoration(
-                        //     border:
-                        //         Border.all(color: Colors.grey[850], width: 2)),
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            tx.title,
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            DateFormat.yMMMd().format(tx.date),
-                            style: TextStyle(
-                              color: Colors.grey[850],
-                              fontSize: 12,
-                            ),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                );
-              },
-            ).toList(),
+            // TransactionList()
           ),
         ],
-      ),
+      )
+  
       // floatingActionButton: FloatingActionButton(
       //   backgroundColor: Colors.red,
       //   onPressed: () {},
