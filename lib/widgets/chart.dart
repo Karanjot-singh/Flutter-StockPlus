2ceaa53f19f0850228b55ec4bfba7de434dd588c
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-07-29 01:32:05 +0530

    minor edits

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,23 +1,38 @@
 import 'package:flutter/material.dart';
 import '../models/Transaction.dart';
-import'package:intl/';
+import 'package:intl/intl.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransacitons;
   Chart(this.recentTransacitons);
 
-  List <Map<String,Object>> get weeklyTx {
-    return List.generate(7, (index){return {'day':};});
-  };
+  List<Map<String, Object>> get weeklyTx {
+    return List.generate(
+      7,
+      (index) {
+        final weekday = DateTime.now().subtract(Duration(days: index));
+        var amountSum = 0.0;
+        for (var i = 0; i < recentTransacitons.length; i++) {
+          if (weekday.day == recentTransacitons[i].date.day &&
+              weekday.month == recentTransacitons[i].date.month &&
+              weekday.year == recentTransacitons[i].date.year) {
+            amountSum++;
+          }
+        }
+
+        return {
+          'day': DateFormat.E(weekday),
+          'amount': amountSum,
+        };
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
       child: Row(
-        children: <Widget>[
-          Column(
-            children: <Widget>[],
-          )
-        ],
+        children: <Widget>[],
       ),
       elevation: 6,
       margin: EdgeInsets.all(20),
