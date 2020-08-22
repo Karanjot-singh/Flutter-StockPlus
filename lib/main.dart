34cb270d7349ceb4e91d0e6e99bde441913161ec
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-08-23 01:27:14 +0530

    Added A basic Login Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'package:Xpense/screens/login_page.dart';
 import 'package:flutter/material.dart';
-import './screens/home_screen.dart';
+// import './screens/home_screen.dart';
 
 //39
 void main() {
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
         ),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
+      home: LoginPage(),
     );
   }
 }
