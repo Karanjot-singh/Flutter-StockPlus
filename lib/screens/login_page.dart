4bf851ba967b16895577b9d223215d9f4486986b
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-08-23 01:18:07 +0530

    Login Screen Added

---
+++ lib/screens/login_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+      ),
+      body: Body(),
+    );
+  }
+}
+
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+        alignment: Alignment.center,
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              TextField(
+                  controller: usernameController,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.person),
+                    labelText: "Username",
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.teal,
+                        width: 5,
+                      ),
+                    ),
+                    suffixIcon: IconButton(
+                      icon: Icon(Icons.check_circle_outline),
+                      splashColor: Colors.blue,
+                      tooltip: "Enter",
+                      onPressed: () {},
+                    ),
+                  )),
+              TextField(
+                controller: passwordController,
+              ),
+            ],
+          ),
+        ));
+  }
+}
