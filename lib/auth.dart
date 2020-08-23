272eebed0ff7ecfc8d8e4e266767facc4f2266bf
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-08-24 02:43:13 +0530

    Added Authentication with google

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,16 +1,37 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-final FirebaseAuth _auth =FirebaseAuth.instance;
+final FirebaseAuth _auth = FirebaseAuth.instance;
 final GoogleSignIn googleSignIn = GoogleSignIn();
 
 //async functions will not be executed asap, won't change or stop others functions
 // that are working on with the application at the moment
 
-Future<FirebaseUser> signInWithUser() async{
-  final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
-  final GoogleSignInAuthentication googleSignInAuthentication = await googleSignIn.authentication;
+Future<FirebaseUser> signInWithGoogle() async {
 
-  final AuthCredential credential = GoogleAuthProvider.getCredential(idToken: googleSignInAuthentication, accessToken: googleSignInAuthentication.accessToken);
+  //google related starts
+  // Let the user sign in , get auth from google and get credential > to firebase, authenticate firebase
+  final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+  final GoogleSignInAuthentication googleSignInAuthentication =
+      await googleSignInAccount.authentication;
+
+  final AuthCredential credential = GoogleAuthProvider.getCredential(
+      idToken: googleSignInAuthentication.idToken,
+      accessToken: googleSignInAuthentication.accessToken);
+   //google related stuff ends
+
   final AuthResult authResult = await _auth.signInWithCredential(credential);
+  final FirebaseUser user = authResult.user;
+
+  assert(!user.isAnonymous);
+  assert(await user.getIdToken() != null);
+
+  final FirebaseUser currentUser = await _auth.currentUser();
+  assert(currentUser.uid == user.uid);
+
+  return user;
+}
+
+void signOutGoogle() async {
+  await googleSignIn.signOut();
 }
